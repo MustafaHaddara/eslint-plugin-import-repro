6c371fa87cf04704c5c8ec462669597e5a360dd2
Mustafa Haddara <mhaddara@pagerduty.com>
2021-07-22 15:39:46 -0400

    add eslint-plugin-import and configure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { nonsense } from 'react';
 import ReactDOM from 'react-dom';
+import { dontCreateStore } from 'redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
